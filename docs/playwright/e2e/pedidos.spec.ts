051b11fd63df95d48aaa274c217fc6f0cecb8303
Diego Monteiro <diego.monteiro007@gmail.com>
2026-03-30 11:58:29 -0300

    Iniciando a automação do testes de consulta de pedido aprovado

---
+++ docs/playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('Deve consultar um pedido após a compra', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+
+  await page.getByTestId('search-order-id').fill('VLO-TQVVAQ');
+
+  await page.getByTestId('search-order-button').click();
+  await expect(page.getByTestId('order-result-id')).toBeVisible();
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-TQVVAQ');
+  await expect(page.getByTestId('order-result-status')).toBeVisible();
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+});
